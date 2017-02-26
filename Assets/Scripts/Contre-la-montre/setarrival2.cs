e5487cecf417f5aeb50fea3c996235cac06c28af
Viincentt <vincent.hu@epita.fr>
2017-02-26 19:40:37 +0100

    26/02 19:40

---
+++ Assets/Scripts/Contre-la-montre/setarrival2.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class setarrival2 : MonoBehaviour {
+
+	int nbrtour = 0;
+	// Use this for initialization
+	/*void Start () {
+	
+	}*/
+
+	// Update is called once per frame
+	/*void Update () {
+
+	}*/
+	public void OnTriggerEnter(Collider other){
+		// nbrtour = 9 pour 3 tours
+		// nbrtour = 6 pour 2 tours
+		// nrbtour = 3 pour 1 tour
+		if (nbrtour < 3) {
+			nbrtour += 1;
+			GameObject.Find ("Car").SendMessage ("Lap");
+		}
+		else
+			GameObject.Find ("Car").SendMessage ("Finnish");
+
+
+	}
+}
