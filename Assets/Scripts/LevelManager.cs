2d8cbe4332b89dfb099874f870d78aaab0fb42b3
bboy45 <vidjekevin@gmail.com>
2017-02-26 18:14:35 +0100

    26/02 18:11

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	public Transform mainMenu, optionsMenu,PlayMenu;
+
+	public void LoadScene(string name){
+		Application.LoadLevel(name);
+	}
+	public void QuitGame(){
+		Application.Quit();
+	}
+	public void OptionsMenu(bool clicked){
+		if (clicked == true){
+			optionsMenu.gameObject.SetActive(clicked);
+			mainMenu.gameObject.SetActive(false);
+		} else {
+			optionsMenu.gameObject.SetActive(clicked);
+			mainMenu.gameObject.SetActive(true);
+		}
+	}
+    public void playMenu(bool clicked)
+    {
+        if (clicked == true)
+        {
+            PlayMenu.gameObject.SetActive(clicked);
+            mainMenu.gameObject.SetActive(false);
+        }
+        else
+        {
+            PlayMenu.gameObject.SetActive(clicked);
+            mainMenu.gameObject.SetActive(true);
+        }
+    }
+
+
+}
